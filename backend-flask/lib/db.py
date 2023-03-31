2f6ca0f4f3d39201ee9faa37340379a77e8661bf
Jaime Pastrian <jaime.pastrian@gmail.com>
2023-03-31 22:50:00 +0000

    [ADD] Finish creating our utility scripts DynamoDB

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -8,11 +8,11 @@ class Db:
     def __init__(self):
         self.init_pool()
 
-    def print_sql(self, title, sql):
+    def print_sql(self, title, sql, params={}):
         cyan = '\033[96m'
         no_color = '\033[0m'
         print(f'{cyan}SQL STATEMENT-[{title}]---{no_color}')
-        print(sql + "\n")
+        print(sql, params)
 
     def template(self, *args):
         pathing = list((app.root_path, 'db', 'sql',) + args)
@@ -36,7 +36,7 @@ class Db:
     # we want to commit data (like insert)
     # RETURNING has to be in Uppercases
     def query_commit(self, sql, params={}):
-        self.print_sql("commit-with-returnin-id", sql)
+        self.print_sql("commit-with-returnin-id", sql, params)
         pattern = r"\bRETURNING\b"
         is_returning_id = re.search(pattern, sql)
         try:
@@ -52,9 +52,19 @@ class Db:
             self.print_sql_err(err)
             #conn.rollback()
 
+    # when we want to return a a single value
+    def query_value(self,sql,params={}):
+        self.print_sql('value',sql,params)
+
+        with self.pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql,params)
+                json = cur.fetchone()
+                return json[0]
+
     # when we want to return a json object
     def query_array_json(self, sql, params={}):
-        self.print_sql("array", sql)
+        self.print_sql("array", sql, params)
 
         wrapped_sql = self.query_wrap_array(sql)
         with self.pool.connection() as conn:
@@ -67,7 +77,7 @@ class Db:
 
     # when we want to return an array of json objects
     def query_object_json(self, sql, params={}):
-        self.print_sql("json", sql)
+        self.print_sql("json", sql, params)
 
         wrapped_sql = self.query_wrap_object(sql)
         with self.pool.connection() as conn:
